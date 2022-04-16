1cc4485abb2f53bf77458a14b4143524801f9dd0
Netson <netsoncavina@gmail.com>
2022-04-16 19:35:51 -0300

    adicionado a configuração inicial

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const multer = require("multer");
+const { TesseractWorker } = require("tesseract.js");
+const worker = new TesseractWorker();
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, res, cb) => {
+    cb(null, res.file);
+  },
+});
+
+const upload = multer({ storage: storage }).single("avatar");
+
+app.set("view engine", "ejs");
