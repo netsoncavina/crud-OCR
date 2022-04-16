614331523425bb27e1147f6dc287c1aa87de1685
Netson <netsoncavina@gmail.com>
2022-04-16 19:49:33 -0300

    adicionado view e setup do servidor

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Imports
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const multer = require("multer");
-const { TesseractWorker } = require("tesseract.js");
-const worker = new TesseractWorker();
+const { createWorker } = require("tesseract.js");
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
 
+// Armazenamento
 const storage = multer.diskStorage({
   destination: (req, res, cb) => {
     cb(null, "./uploads");
@@ -13,7 +17,19 @@ const storage = multer.diskStorage({
     cb(null, res.file);
   },
 });
-
-const upload = multer({ storage: storage }).single("avatar");
+const upload = multer({ storage: storage }).single("file");
 
 app.set("view engine", "ejs");
+
+// Rotas
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Start server
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
+// app.get("/uploads", (req, res) => {
+//   console.log("hey");
+// });
